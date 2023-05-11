2d4fbff020393902e7e936d05bcc1b7aa1baf499
Avrey156 <108403904+Avrey156@users.noreply.github.com>
2023-05-12 01:57:30 +0700

    page offer (flash sale)

--- public/js/script.js
+++ public/js/script.js
@@ -95,3 +95,14 @@ var productBig = new Swiper ('.big-image', {
         swiper: productThumb,
     }
 });
+
+// product stock bar width (percentage)
+var stocks = document.querySelectorAll('.products .stock');
+for (let x = 0; x < stocks.length; x++) {
+    let stock = stocks[x].dataset.stock,
+    available = stocks[x].querySelector('.qty-available').innerHTML,
+    sold = stocks[x].querySelector('.qty-sold').innerHTML,
+    percent = sold*100/stock;
+
+    stocks[x].querySelector('.available').style.width = percent + '%';
+}
