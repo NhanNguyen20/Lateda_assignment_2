ff94cc6e450493f904539f9de8904c74001ae458
Nguyen Le Thu Nhan <102719300+NhanNguyen20@users.noreply.github.com>
2023-05-14 00:32:30 +0700

    function for customer register and view account

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = 3000;
 // Import Bcrypt Module
 const bcrypt = require('bcrypt');
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: 'public/assets/profilePics',
+  filename: function(req, file, cb) {
+    cb(null, file.originalname); // set the filename as the original name of the uploaded file
+  }
+});
+const upload = multer({ storage: storage });
+
 // Import Model Modules
 const Product = require('./model/Product');
 const Customer = require('./model/Customer');
@@ -18,15 +27,30 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Customer Registration
-app.post('/customer/register', (req, res) => {
+app.post('/customer/register', upload.single('profilePicture'), (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   // const hashedPassword = await bcrypt.hash
-  const customer = new Customer(req.body)
+  const customer = new Customer({
+    ...req.body,
+    profilePicture: req.file.filename
+  });
   customer.save()
   .then((customer) => {res.send(customer)}) // Test
   .catch((error) => {res.send(error.message)})
-})
+});
+
+// My Account for Customer
+app.get('/customer/:id', (req, res) => {
+  Customer.findById(req.params.id)
+    .then((customer) => {
+      if (!customer) {
+        return res.send("Cannot found customer ID!");
+      }
+      res.render('my-account', {customer});
+    })
+    .catch((error) => res.send(error));
+});
 
 // Vendor Registration
 app.post('/vendor/register', (req, res) => {
