3a959cd6d213b562d4237caf5682340e49bd569f
Nguyen Le Thu Nhan <102719300+NhanNguyen20@users.noreply.github.com>
2023-05-20 06:15:23 +0700

    add file headers

--- model/Order.js
+++ model/Order.js
@@ -1,3 +1,16 @@
+// RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023A
+// Assessment: Assignment 2
+// Authors: 
+// Nguyen Le Thu Nhan - s3932151
+// Ong Gia Man - s3938231
+// Nguyen Ngoc Minh Thu - s3941327
+// Nguyen Tuan Duong - s3965530
+// Nguyen Pham Tien Hai - s3979239
+// Chau The Kien - s3790421
+// Acknowledgement: Acknowledge the resources that you use here.
+
 const mongoose = require('mongoose');
 const db = require('./db');
 const Product = require('./Product');
--- model/db.js
+++ model/db.js
@@ -1,4 +1,17 @@
-// this file is used to connect to the database
+// RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023A
+// Assessment: Assignment 2
+// Authors: 
+// Nguyen Le Thu Nhan - s3932151
+// Ong Gia Man - s3938231
+// Nguyen Ngoc Minh Thu - s3941327
+// Nguyen Tuan Duong - s3965530
+// Nguyen Pham Tien Hai - s3979239
+// Chau The Kien - s3790421
+// Acknowledgement: Acknowledge the resources that you use here.
+
+// This file is used to connect to the database
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://user:mypassword@lateda.iejgaoj.mongodb.net/lateda-database?retryWrites=true&w=majority')
